06bfb3cd502e3d1f6f70af721ad037b5eadffc5a
nalawadekrushna26 <nalawadekrushna26@gmail.com>
2026-05-24 15:40:35 +0530

    Added script.js

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
 document.getElementById("btn").addEventListener("click", function () {
     alert("JavaScript is connected!");
 });
+
+const apiKey = "cf61723150b20c21ea3fc1f544c4cac2";
+
+function getWeather() {
+
+  const city = document.getElementById("city").value;
+
+  const url =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+      // Log API response
+      console.log(data);
+
+    })
+    .catch(error => {
+
+      console.log("Error fetching weather data:", error);
+
+    });
+}
