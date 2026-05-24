c317f3c3376465a9be39f16b6c77c442111a27c9
nalawadekrushna26 <nalawadekrushna26@gmail.com>
2026-05-24 16:31:58 +0530

    updated script file

--- script.js
+++ script.js
@@ -25,3 +25,26 @@ function getWeather() {
 
     });
 }
+
+const apiKey = "cf61723150b20c21ea3fc1f544c4cac2";
+
+function getWeather() {
+
+  const city = document.getElementById("city").value;
+
+  const url =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+
+      console.log(data); // for debugging
+
+      displayWeather(data);
+
+    })
+    .catch(error => {
+      console.log("Error:", error);
+    });
+}
